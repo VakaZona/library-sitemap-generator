111641d2308e6e4425f681bbfae3c82e37959252
Valery Zonov <valery.zonov@sibers.com>
2024-05-12 00:28:38 +0700

    add generate XML file

--- src/SitemapGenerator.php
+++ src/SitemapGenerator.php
@@ -19,9 +19,9 @@ class SitemapGenerator
     public function generateSitemap()
     {
         switch ($this->data->fileType) {
-            //            case 'xml':
-            //                $this->generateXMLSitemap();
-            //                break;
+            case 'xml':
+                $this->generateXMLSitemap();
+                break;
             case 'csv':
                 $this->generateCSVSitemap();
                 break;
@@ -73,6 +73,35 @@ class SitemapGenerator
         file_put_contents($filePath, $jsonContent);
     }
 
+    private function generateXMLSitemap()
+    {
+        $filePath = $this->data->filePath;
+        $this->checkFile($filePath);
+
+        $xmlDoc = new \DOMDocument('1.0', 'UTF-8');
+
+        $urlset = $xmlDoc->createElement('urlset');
+        $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+        $urlset->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $urlset->setAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+
+        foreach ($this->data->pages as $page) {
+            $url = $xmlDoc->createElement('url');
+
+            $url->appendChild($xmlDoc->createElement('loc', $page['loc']));
+            $url->appendChild($xmlDoc->createElement('lastmod', $page['lastmod']));
+            $url->appendChild($xmlDoc->createElement('priority', $page['priority']));
+            $url->appendChild($xmlDoc->createElement('changefreq', $page['changefreq']));
+
+            $urlset->appendChild($url);
+        }
+
+        $xmlDoc->appendChild($urlset);
+
+        $xmlDoc->formatOutput = true;
+        $xmlDoc->save($filePath);
+    }
+
     private function checkFile(string $filePath): void
     {
         $parts = explode(DIRECTORY_SEPARATOR, $filePath);
@@ -89,4 +118,5 @@ class SitemapGenerator
             touch($filePath);
         }
     }
+
 }
